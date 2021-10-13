d8fd57dc770aea6572f18e477960e6b091442eb6
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2021-10-13 19:02:14 +0200

    First Commit

---
+++ Flocking/Assets/Flock.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockingManager myManager;
+    Vector3 direction;
+    float speed;
+    float freq;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((myManager.transform.position - transform.position).magnitude > myManager.swimLimit)
+        {
+            direction = (myManager.transform.position - transform.forward).normalized * speed;
+            Debug.Log((myManager.transform.position - transform.position).normalized);
+        }
+        else
+        {
+            direction = (Cohesion() + Align() + Separation()).normalized * speed;
+            Debug.Log((Cohesion() + Align() + Separation()).normalized);
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+        transform.Translate(Time.deltaTime * speed, 0.0f, 0.0f);
+    }
+
+    Vector3 Cohesion()
+    {
+        Vector3 cohesion = Vector3.zero;
+        int num = 0;
+        foreach (GameObject go in myManager.allFish)
+        {
+            if (go != this.gameObject)
+            {
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+                if (distance <= myManager.neighbourDistance)
+                {
+                    cohesion += go.transform.position;
+                    num++;
+                }
+            }
+        }
+        if (num > 0)
+            cohesion = (cohesion / num - transform.position).normalized * speed;
+
+        return cohesion;
+    }
+
+    Vector3 Align()
+    {
+        Vector3 align = Vector3.zero;
+        int num = 0;
+        foreach (GameObject go in myManager.allFish)
+        {
+            if (go != this.gameObject)
+            {
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+                if (distance <= myManager.neighbourDistance)
+                {
+                    align += go.GetComponent<Flock>().direction;
+                    num++;
+                }
+            }
+        }
+        if (num > 0)
+        {
+            align /= num;
+           speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
+        }
+        return align;
+    }
+    Vector3 Separation()
+    {
+        Vector3 separation = Vector3.zero;
+        foreach (GameObject go in myManager.allFish)
+        {
+            if (go != this.gameObject)
+            {
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+                if (distance <= myManager.neighbourDistance)
+                    separation -= (transform.position - go.transform.position) / (distance * distance);
+            }
+        }
+        return separation;
+    }
+}
---
+++ Flocking/Assets/FlockingManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class FlockingManager : MonoBehaviour
+{
+    public GameObject fishPrefab;
+    public int numFish;
+    public Vector3 minPosition;
+    public Vector3 maxPosition;
+    public GameObject[] allFish;
+    public Transform acumulator;
+    public float neighbourDistance;
+    [Range(0f, 10f)]
+    public float minSpeed;
+    [Range(0f, 10f)]
+    public float maxSpeed;
+    [Range(0f, 10f)]
+    public float rotationSpeed;
+
+    public int swimLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 random = new Vector3(Random.Range(minPosition.x, maxPosition.x), Random.Range(minPosition.y, maxPosition.y), Random.Range(minPosition.z, maxPosition.z));
+            Vector3 pos = this.transform.position + random;
+            Vector3 randomize = random;
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.LookRotation(randomize), acumulator);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
